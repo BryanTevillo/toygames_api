9a1a6bb05389b4d4cc97eeebd8fe17fdb0b76576
Bryan Tevillo Betancourt <bbetancourt@2806-105e-0026-5dff-17e1-b8c1-efc8-58ff.ipv6.infinitum.net.mx>
2026-09-18 17:30:01 -0600

    feature: Se agrega el dominio de operations terminado

--- src/features/operations/domain.rs
+++ src/features/operations/domain.rs
@@ -20,4 +20,20 @@ pub enum OperationType {
 }
 
 #[derive(Debug, FromRow, Serialize, Deserialize)]
-pub struct MachineOperation {}
+pub struct MachineOperation {
+    pub id: Uuid,
+    pub machine_id: Uuid,
+    pub operation_type: OperationType,
+    pub status: OperationStatus,
+    pub performed_at: NaiveDate,
+    pub audited_at: Option<NaiveDate>,
+    pub system_coins: i16,
+    pub system_prize_out: i16,
+    pub lcd_img_url: String,
+    pub physical_toys_count: i16,
+    pub toys_added: i16,
+    pub client_payout: Option<f32>,
+    pub cash_withdrawn: Option<f32>,
+    pub company_profit: Option<f32>,
+    pub cash_discrepancy: Option<f32>,
+}
